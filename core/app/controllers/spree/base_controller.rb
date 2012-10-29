753bb2654e96354e35cbe41d7c8fb478c63c6c26
Ryan Bigg <radarlistener@gmail.com>
2012-10-29 14:50:58 +1100

    Remove respond_with helper

--- core/app/controllers/spree/base_controller.rb
+++ core/app/controllers/spree/base_controller.rb
@@ -2,7 +2,5 @@ require 'cancan'
 
 class Spree::BaseController < ApplicationController
   include Spree::Core::ControllerHelpers::Auth
-  include Spree::Core::ControllerHelpers::RespondWith
   include Spree::Core::ControllerHelpers::Common
-
 end
