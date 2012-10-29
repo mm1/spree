de0cfcc7aa7692914b40eb29732c1c21287ffce6
Ryan Bigg <radarlistener@gmail.com>
2012-10-29 12:24:49 +1100

    Remove require for spree/core/responder

--- core/lib/spree/core.rb
+++ core/lib/spree/core.rb
@@ -68,7 +68,6 @@ end
 
 require 'spree/core/delegate_belongs_to'
 
-require 'spree/core/responder'
 require 'spree/core/ssl_requirement'
 require 'spree/core/store_helpers'
 require 'spree/core/calculated_adjustments'
