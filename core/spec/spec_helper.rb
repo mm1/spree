7acbac6320ae94f8c33d67d1238da75a5dd30531
Ryan Bigg <radarlistener@gmail.com>
2012-11-09 16:52:33 +1100

    Fix references to testing_support within core

--- core/spec/spec_helper.rb
+++ core/spec/spec_helper.rb
@@ -10,8 +10,8 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
 require 'database_cleaner'
 
-require 'spree/models/testing_support/factories'
-require 'spree/models/testing_support/preferences'
+require 'spree/testing_support/factories'
+require 'spree/testing_support/preferences'
 
 require 'spree/testing_support/controller_requests'
 require 'spree/testing_support/authorization_helpers'
@@ -50,7 +50,7 @@ RSpec.configure do |config|
 
   config.include FactoryGirl::Syntax::Methods
 
-  config.include Spree::Models::TestingSupport::Preferences
+  config.include Spree::TestingSupport::Preferences
 
   config.include Spree::TestingSupport::UrlHelpers
   config.include Spree::TestingSupport::ControllerRequests
